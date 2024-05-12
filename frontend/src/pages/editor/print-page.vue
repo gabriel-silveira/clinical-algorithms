d8a16dcd37d864543c5559f7e6278646f21bfe9a
Gabriel Silveira <gabriel@gabrielsilveira.com.br>
2024-05-12 13:54:00 -0300

    Rollback

--- frontend/src/pages/editor/print-page.vue
+++ frontend/src/pages/editor/print-page.vue
@@ -3,14 +3,6 @@
     id="editor"
     class="print-mode bg-grey-4 overflow-hidden absolute-top-left full-width full-height"
   >
-    <div class="print-mode-header z-max">
-      <div class="title">{{ algorithm.title }}</div>
-
-      <div class="creation-creation">{{ algorithm.description }}</div>
-
-      <div>Última actualización: {{ formatDatetime(algorithm.updated_at) }}</div>
-    </div>
-
     <div
       id="editor-content" class="bg-white overflow-auto full-height"
     >
@@ -33,7 +25,6 @@ const route = useRoute();
 import EditorStage from 'components/editor/editor-stage.vue';
 import Editor from 'src/services/editor';
 import { GRAPH_MODE_PRINT } from 'src/services/editor/types';
-import { formatDatetime } from 'src/services/date';
 
 const editor = inject('editor') as Editor;
 
